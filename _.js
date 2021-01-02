6758630ce43d822a4967415e7d2edfb43a4bd6c6
jdlawton <joe.lawton@outlook.com>
2021-01-02 12:04:34 -0600

    Continuing through exercises. Finished String and Object exercises.

--- _.js
+++ _.js
@@ -14,14 +14,47 @@ const _ = {
             start = end;
             end = temp;
         }
-
         if (number < start || number >= end) {
             return false;
         } else {
             return true;
         }
     },
-    
+    words: function (string) {
+        return string.split(' ');
+    },
+    pad: function (string, length) {
+        if (string.length >= length){
+            return string;
+        }
+        beginningPad = Math.floor((length - string.length) / 2);
+        endPad = length - string.length - beginningPad;
+        let paddedString = `${' '.repeat(beginningPad)}${string}${' '.repeat(endPad)}`;
+        return paddedString;
+    },
+    has: function (object, key) {
+        if (object[key]) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    invert: function (object) {
+        let invertedObject = {};
+        for (const property in object) {
+            let originalValue = object[property];
+            invertedObject.originalValue = property;
+        }
+        return invertedObject;
+    },
+    findKey: function (object, predicate) {
+        for (const property in object) {
+            if (predicate(object[property])) {
+                return property;
+            }
+        }
+        return undefined;
+    }
 };
 
 
